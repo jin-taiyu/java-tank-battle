cf73538578ce2eb98e6fb51c148802e4f8e424eb
Jin Taiyu <60456972+jin-taiyu@users.noreply.github.com>
2025-04-19 16:33:05 +0800

    Fix the apache problem

--- src/main/java/module-info.java
+++ src/main/java/module-info.java
@@ -4,6 +4,16 @@ module com.tankbattle {
     requires javafx.media;
     requires javafx.swing;
     
+    requires java.desktop;
+    
+    requires transitive batik.all;
+    
     exports com.tankbattle;
+    exports com.tankbattle.model;
+    exports com.tankbattle.controller;
+    exports com.tankbattle.view;
+    
     opens com.tankbattle to javafx.fxml;
+    opens com.tankbattle.view to javafx.fxml;
+    opens com.tankbattle.controller to javafx.fxml;
 }
